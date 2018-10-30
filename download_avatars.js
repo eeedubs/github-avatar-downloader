e42de198cc2e7917f4054626bba883118a0c0895
Eric Wilson <ericswilson94@gmail.com>
2018-10-29 19:48:31 -0700

    updated changes

--- download_avatars.js
+++ download_avatars.js
@@ -1,12 +1,33 @@
 var request = require('request');
+require('dotenv').config();
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
-function getRepoContributors(repoOwner, repoName, cb) {
-  //
+function getRepoContributors(repoOwner, repoName, handleRequest) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    qs: {
+      "access_token": process.env.GITHUB_TOKEN
+    },
+    headers: {
+      'User-Agent': 'request',
+    }
+  };
+// loads the list of contributors via HTTPS for the given repo
+
+  request(options, handleRequest);
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
+  if (err){
+    throw err;
+  }
+  var contributors = JSON.parse(result.body);
+  // body is the information that I requested
+  // JASON.parse converts a JSON string into javascript
+  contributors.forEach(function (contributor){
+    console.log(contributor.avatar_url);
+  })
+  // console.log("Errors:", err);
+  // console.log("Result:", contributors);
 });
